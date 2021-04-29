3681fd9a02adf007294acb2ab7bfd13e557e3706
TheGameVlog <initiotech@gmail.com>
2021-04-30 02:15:14 +0530

    Add project files.

---
+++ FNKeychain/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtGUID = New System.Windows.Forms.TextBox()
+        Me.txtAES = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtKeychain = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(198, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fortnite Keychain Generator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter AES :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Enter GUID :"
+        '
+        'txtGUID
+        '
+        Me.txtGUID.Location = New System.Drawing.Point(104, 64)
+        Me.txtGUID.Name = "txtGUID"
+        Me.txtGUID.Size = New System.Drawing.Size(419, 23)
+        Me.txtGUID.TabIndex = 3
+        '
+        'txtAES
+        '
+        Me.txtAES.Location = New System.Drawing.Point(104, 93)
+        Me.txtAES.Name = "txtAES"
+        Me.txtAES.Size = New System.Drawing.Size(419, 23)
+        Me.txtAES.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(237, 137)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtKeychain
+        '
+        Me.txtKeychain.BackColor = System.Drawing.Color.White
+        Me.txtKeychain.Location = New System.Drawing.Point(34, 196)
+        Me.txtKeychain.Name = "txtKeychain"
+        Me.txtKeychain.ReadOnly = True
+        Me.txtKeychain.Size = New System.Drawing.Size(489, 23)
+        Me.txtKeychain.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(234, 173)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "New Keychain"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(163, 304)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(223, 15)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Currently available Keychains in Nitestats"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.White
+        Me.RichTextBox1.Location = New System.Drawing.Point(33, 332)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(489, 435)
+        Me.RichTextBox1.TabIndex = 9
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(208, 266)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(127, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Load NiteStats Keychains"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Enabled = False
+        Me.btnAdd.Location = New System.Drawing.Point(34, 225)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(127, 23)
+        Me.btnAdd.TabIndex = 11
+        Me.btnAdd.Text = "Add to List"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(208, 779)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(127, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Send to Pastebin"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(551, 814)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtKeychain)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtAES)
+        Me.Controls.Add(Me.txtGUID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "FN Keychain Updater"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtGUID As TextBox
+    Friend WithEvents txtAES As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtKeychain As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ FNKeychain/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports Json.Net
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.ComponentModel
+Imports System.Net
+Imports System.IO
+Imports System.Collections.Specialized
+
+
+Public Class Form1
+
+    Dim base64Decoded As String = "base64 encoded string"
+    Dim base64Encoded As String
+    Dim data As Byte()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If txtGUID.Text = "" Then
+            txtGUID.Focus()
+            ErrorProvider1.SetError(txtGUID, "Cannot Be Left Blank")
+            Exit Sub
+        End If
+        If txtAES.Text = "" Then
+            txtAES.Focus()
+            ErrorProvider1.SetError(txtAES, "Cannot Be Left Blank")
+            Exit Sub
+        End If
+        Try
+            txtKeychain.Text = txtGUID.Text & ":" & TheEncoder.HexStringToBase64(txtAES.Text.TrimStart(New Char() {"0", "x"}))
+        Catch ex As Exception
+            ErrorProvider1.SetError(txtAES, "Invalid AES Key")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub FetchKeychainsFromNiteStats()
+        Dim connectNite As New WebClient()
+        Dim jData = connectNite.DownloadString("https://api.nitestats.com/v1/epic/keychain")
+
+        Dim jTok As JToken = JToken.Parse(jData)
+
+        RichTextBox1.Text = jTok.ToString(Formatting.Indented)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FetchKeychainsFromNiteStats()
+        If RichTextBox1.Text <> "" Then
+            btnAdd.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim sInt As Integer = RichTextBox1.Find(txtKeychain.Text)
+        If sInt <> -1 Then
+            RichTextBox1.SelectionStart = sInt
+            RichTextBox1.SelectionLength = txtKeychain.Text.Length
+            RichTextBox1.SelectionBackColor = Color.Red
+            RichTextBox1.ScrollToCaret()
+        Else
+            RichTextBox1.SelectionStart = RichTextBox1.Text.IndexOf("[") + 1
+            RichTextBox1.SelectionLength = 1
+            RichTextBox1.SelectedText = Environment.NewLine & """" & txtKeychain.Text & """," & Environment.NewLine
+            RichTextBox1.SelectionStart = 0
+            RichTextBox1.ScrollToCaret()
+        End If
+    End Sub
+
+
+    Private Sub txtGUID_Validating(sender As Object, e As CancelEventArgs) Handles txtGUID.Validating, txtAES.Validating
+        If sender.text <> "" Then
+            ErrorProvider1.SetError(sender, "")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim pasteClient As New WebClient()
+
+        Dim pasteRequest As New NameValueCollection
+        pasteRequest.Add("api_dev_key", "<<PASTE BIN DEVELOPER KEY>>") 'Simply login to Pastebin and go to https://pastebin.com/doc_api#1
+        pasteRequest.Add("api_option", "paste")
+        pasteRequest.Add("api_paste_code", RichTextBox1.Text)
+
+        Dim UploadPasteData = pasteClient.UploadValues("https://pastebin.com/api/api_post.php", pasteRequest)
+
+        Dim responsePasteBin As String = New StreamReader(New MemoryStream(UploadPasteData)).ReadToEnd()
+
+        Dim pasteID = responsePasteBin.Substring(responsePasteBin.LastIndexOf("/") + 1)
+
+        Dim openPage As New ProcessStartInfo
+        openPage.UseShellExecute = True
+        openPage.FileName = "https://pastebin.com/raw/" & pasteID
+
+        Process.Start(openPage)
+
+
+    End Sub
+End Class
+
+Public Class TheEncoder
+    Public Shared Function HexStringToBase64(ByVal AES As String) As String
+        Dim data As Byte() = ConvertFromStringToHex(AES)
+        Return Convert.ToBase64String(data)
+    End Function
+
+    Public Shared Function ConvertFromStringToHex(ByVal AES As String) As Byte()
+        AES = AES.Replace("-", "")
+
+        Return Enumerable.Range(0, AES.Length / 2).[Select](Function(x) Convert.ToByte(AES.Substring(x * 2, 2), 16)).ToArray()
+    End Function
+
+End Class
